35e2cb3e884697a58c498441f978dedb51f46f3c
Fredrik Bjarnemar <fredrik.bjarnemar@gmail.com>
2023-04-03 09:14:58 +0200

    vet ej

---
+++ 00-Python Object and Data Structure Basics/test.py
@@ -0,0 +1,8 @@
+# with open('test2.txt',mode='w') as f:
+#   f.write('Hello World')
+#   f.close()
+
+
+with open('test2.txt',mode='r') as f:
+    print(f.read())
+
---
+++ Mine/test.py
@@ -0,0 +1,13 @@
+#hungrig = True
+#if hungrig:
+#    print ("Rätt")
+#else:
+#    print ("Fel")
+
+#loc = 'Bank'
+#if loc == 'Bilmek':
+#    print('bilar är coola')
+
+x = set('Missiiisippiiimmmmmm')
+print (x)
+
